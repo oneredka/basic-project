e9425298283dc1891da1578525d8244cb64c2288
louis.yi <louis.yi@qima.com>
2022-10-20 15:19:48 +0800

    update sql

--- basic-project/icarus-web/src/main/java/com/hong/icarus/web/user/UserController.java
+++ basic-project/icarus-web/src/main/java/com/hong/icarus/web/user/UserController.java
@@ -6,10 +6,7 @@ import com.hong.biz.user.service.ISysUserService;
 import com.hong.common.core.controller.BaseController;
 import com.hong.common.core.page.TableDataInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,14 +17,14 @@ public class UserController extends BaseController {
     @Autowired
     private ISysUserService userService;
 
-    @PostMapping("/get_user_by_id")
+    @GetMapping("/get_user_by_id")
     public SysUser getUserById(String id) {
         SysUser user = userService.getUserById(id);
         System.out.println(user.toString());
         return user;
     }
 
-    @PostMapping("/get_all_user")
+    @GetMapping("/get_all_user")
     @ResponseBody
     public TableDataInfo getAllUser() {
         startPage();
